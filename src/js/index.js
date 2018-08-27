b9e59848b87c64f3ac4513bc31ec4127b1c697f5
Alex Lazareva <alex.lazareva25@yandex.ru>
2018-08-27 21:51:13 +0300

    add and include vk.api to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,3 @@
 import '../scss/main.scss';
+
+const api = require('./modules/vk.api.js');
---
+++ src/js/modules/vk.api.js
@@ -0,0 +1,11 @@
+module.exports = (params, method) => {
+    return new Promise((resolve, reject) => {
+        VK.api(method, params, data => {
+            if (data.error) {
+                reject(new Error(data.error.error_msg));
+            } else {
+                resolve(data.response);
+            }
+        });
+    });
+};

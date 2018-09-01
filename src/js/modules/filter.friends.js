aa83b03670e1a9e593e22c48c6491ea9cb185544
Alex Lazareva <alex.lazareva25@yandex.ru>
2018-09-01 23:38:06 +0300

    fix filter

--- src/js/modules/filter.friends.js
+++ src/js/modules/filter.friends.js
@@ -4,20 +4,20 @@ module.exports = () => {
             if (event.target.classList.contains('form__search')) {
                 let target = event.target,
                     value = event.target.value.trim().toLowerCase(),
-                    list = target.parentNode.parentNode.querySelector('.friends__list');
-                 // let items = list.children;
+                    block = target.parentNode.parentNode.nextElementSibling.children[1],
+                    list = block.firstChild.nextSibling,
+                    items = list.children;
 
-                // for (let i = 0, length = items.length; i < length; i++) {
-                //     let item = items[i],
-                //         name = item.querySelector('.friend__name');
-                //
-                //     if (name.includes(value)) {
-                //         item.style.display = 'block';
-                //     } else {
-                //         item.style.display = 'none';
-                //     }
-                // }
-                console.log(list);
+                for (let i = 0, length = items.length; i < length; i++) {
+                    let item = items[i],
+                        name = item.querySelector('.friend__name').textContent.toLowerCase();
+
+                    if (name.includes(value)) {
+                        item.style.display = 'block';
+                    } else {
+                        item.style.display = 'none';
+                    }
+                }
             }
         }
     }
